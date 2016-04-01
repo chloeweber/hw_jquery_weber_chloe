028f3bd8be62c8da3ec977a7406a3c1217717362
Chloe Weber <chloeesierraa@yahoo.com>
2016-04-01 14:56:45 -0500

    Add headings code and add jQuery link

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-$( document ).ready( function( ) {
-
+$( document ).ready( function() {
+    var headings = $( 'h2' );
+    headings.last().text( 'I am Chloe Weber, and I am learning jQuery!' );
 } );
